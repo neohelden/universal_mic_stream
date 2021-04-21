16b81148000aeefbc32a282b7264aaca46f3cf80
Maik Hummel <maik@neohelden.com>
2021-04-21 12:12:26 +0200

    Interims tries

--- universal_mic_stream_web/lib/universal_mic_stream_web.dart
+++ universal_mic_stream_web/lib/universal_mic_stream_web.dart
@@ -8,6 +8,7 @@ import 'package:http/http.dart' as http;
 import 'dart:web_audio';
 import 'dart:math' as Math;
 import 'package:universal_mic_stream_platform_interface/universal_mic_stream_platform_interface.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class UniversalMicStreamPlugin extends UniversalMicStreamPlatform {
 
@@ -16,7 +17,7 @@ class UniversalMicStreamPlugin extends UniversalMicStreamPlatform {
   MediaStreamAudioSourceNode? audioInput;
   MediaStream? mediaStream;
   bool _isRecording = false;
-
+  BytesBuilder completeDataStream = BytesBuilder();
 
   /// Registers this class as the default instance of [UniversalMicStreamPlatform].
   static void registerWith(Registrar registrar) {
@@ -55,12 +56,14 @@ class UniversalMicStreamPlugin extends UniversalMicStreamPlatform {
       audioInput!.connectNode(recorder!);
       recorder!.onAudioProcess.listen((AudioProcessingEvent e) {
         var source = e.inputBuffer!.getChannelData(0);
-        var downSampled = resample(
-            buffer: source, srcSampleRate: sampleRate, destSampleRate: 16000);
 
-        var bytes = downSampled.buffer.asUint8List();
+        // var downSampled = resample(
+        //     buffer: source, srcSampleRate: sampleRate, destSampleRate: 16000);
 
-        _streamController?.add(bytes);
+        // var bytes = downSampled.buffer.asUint8List();
+
+        completeDataStream.add(source.buffer.asUint8List());
+        _streamController?.add(source.buffer.asUint8List());
       });
 
       return _streamController!.stream;
@@ -104,6 +107,18 @@ class UniversalMicStreamPlugin extends UniversalMicStreamPlatform {
     print("Stop recording");
     recorder?.disconnect();
     recorder = null;
+
+    var audioContext = AudioContext();
+    var source = audioContext.createBufferSource();
+    print(completeDataStream.toBytes());
+    var wav = await _writeWavFile(data: completeDataStream.toBytes());
+    source.buffer = await audioContext.decodeAudioData(wav.buffer);
+    // connect the AudioBufferSourceNode to the
+    // destination so we can hear the sound
+    source.connectNode(audioContext.destination!);
+    // start the source playing
+    source.start();
+
     _streamController?.close();
     audioInput?.disconnect();
     if (mediaStream != null) {
@@ -118,4 +133,57 @@ class UniversalMicStreamPlugin extends UniversalMicStreamPlatform {
     _isRecording = false;
     print("Recording stopped");
   }
+
+  Future<Uint8List> _writeWavFile({
+  required List<int> data,
+}) async {
+  var subChunk2Size = data.length;
+  var subChunk1Size = 16;
+  var numChannels = 1;
+  var sampleRate = 16000;
+  var byteRate = sampleRate * numChannels * 2;
+  var fileSize = 4 + (8 + subChunk1Size) + (8 + subChunk2Size);
+
+  var dataWithHeader = Uint8List.fromList([
+    // "RIFF"
+    82, 73, 70, 70,
+    fileSize & 0xff,
+    (fileSize >> 8) & 0xff,
+    (fileSize >> 16) & 0xff,
+    (fileSize >> 24) & 0xff,
+    // WAVE
+    87, 65, 86, 69,
+    // fmt
+    102, 109, 116, 32,
+    // fmt chunk size 16
+    16, 0, 0, 0,
+    // Type of format
+    1, 0,
+    // One channel
+    numChannels, 0,
+    // Sample rate
+    sampleRate & 0xff,
+    (sampleRate >> 8) & 0xff,
+    (sampleRate >> 16) & 0xff,
+    (sampleRate >> 24) & 0xff,
+    // Byte rate
+    byteRate & 0xff,
+    (byteRate >> 8) & 0xff,
+    (byteRate >> 16) & 0xff,
+    (byteRate >> 24) & 0xff,
+    // Uhm
+    2, 0, // (chunkSize * numChannels) >> 3, 0,
+    // bitsize
+    subChunk1Size, 0,
+    // "data"
+    100, 97, 116, 97,
+    subChunk2Size & 0xff,
+    (subChunk2Size >> 8) & 0xff,
+    (subChunk2Size >> 16) & 0xff,
+    (subChunk2Size >> 24) & 0xff,
+    ...data
+  ]);
+
+  return dataWithHeader;
+}
 }
